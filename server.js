88271febdd3acde37a5c298c77dd50dd59e39192
Zacchaeus Aladeokin <110726099+zaladeokin@users.noreply.github.com>
2024-03-12 20:35:09 +0100

    Update server.js(hostname@server.listen removed)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const url = require('url');
 const { getComments, searchVideo }= require("./scraping");
 
 //const hostname = "web-scrapper-g746.onrender.com";
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -57,5 +57,6 @@ const server= http.createServer(requestHandler);
 
 //Initiaize server to listen
 server.listen(PORT, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    //console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at port: ${PORT}`);
 });
